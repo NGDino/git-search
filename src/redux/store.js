b22ebaa5c5b3ff80b56cd39055b600c7ac85c0d8
Neil Dino <N.dino90@gmail.com>
2021-06-25 11:40:45 -0600

    Delete src/redux directory

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-// import { configureStore } from '@reduxjs/toolkit'
-// import searchParamsReducer from './searchParamsSlice'
-
-// export default configureStore({
-//     reducer: {
-//         searchParams: searchParamsReducer
-//     },
-// })
